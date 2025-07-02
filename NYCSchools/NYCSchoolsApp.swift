f97f965deecc5139c9eba147ae5ef7a1b39890db
Madhav Gadve <maddymadhav1994@gmail.com>
2025-07-02 16:15:24 -0500

    Initial Commit

---
+++ NYCSchools/NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCSchoolsApp.swift
+//  NYCSchools
+//
+//  Created by Madhav Gadve on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
